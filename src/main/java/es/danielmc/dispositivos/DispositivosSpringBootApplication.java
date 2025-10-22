d54b38f974a38fa5fea6caea5d7ecd79c263510e
Daniel Martin <daniel.martin48@educa.madrid.org>
2025-10-22 17:36:40 +0200

    aa

---
+++ src/main/java/es/danielmc/dispositivos/DispositivosSpringBootApplication.java
@@ -0,0 +1,22 @@
+package es.danielmc.dispositivos;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@Slf4j
+@SpringBootApplication
+@EnableCaching
+public class DispositivosSpringBootApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DispositivosSpringBootApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Aplicación iniciada correctamente");
+    }
+}
---
+++ src/test/java/es/danielmc/disexis/DisexisApplicationTests.java
@@ -0,0 +1,13 @@
+package es.danielmc.disexis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisexisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
